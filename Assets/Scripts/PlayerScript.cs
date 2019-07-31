331fdb5c08d38eed2a9b1184a4869ed2aa225766
Marcus Mouyat <marcus@mouyat.com>
2019-07-31 12:10:18 +1000

    Added files to repository

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerScript : NetworkBehaviour
+{
+    public float movementSpeed = 10;
+    public float rotationSpeed = 10;
+    public float jumpHeight = 2;
+    bool isGrounded = false;
+    private Rigidbody rigid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        AudioListener al = GetComponentInChildren<AudioListener>();
+        Camera c = GetComponentInChildren<Camera>();
+        if (isLocalPlayer)
+        {
+            c.enabled = true;
+            al.enabled = true;
+        }
+        else
+        {
+            c.enabled = false;
+            al.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            HandleInput();
+        }
+    }
+
+    void HandleInput()
+    {
+        KeyCode[] keys =
+        {
+            KeyCode.W,
+            KeyCode.S,
+            KeyCode.A,
+            KeyCode.D,
+            KeyCode.Space
+        };
+
+        foreach (var key in keys)
+        {
+            if (Input.GetKey(key))
+            {
+                Move(key);
+            }
+        }
+    }
+
+    void Move(KeyCode _key)
+    {
+        Vector3 position = rigid.position;
+        Quaternion rotation = rigid.rotation;
+        switch(_key)
+        {
+            case KeyCode.W:
+                position += transform.forward * movementSpeed * Time.deltaTime;
+                break;
+            case KeyCode.S:
+                position -= transform.forward * movementSpeed * Time.deltaTime;
+                break;
+            case KeyCode.A:
+                rotation *= Quaternion.AngleAxis(-rotationSpeed, Vector3.up);
+                break;
+            case KeyCode.D:
+                rotation *= Quaternion.AngleAxis(rotationSpeed, Vector3.up);
+                break;
+            case KeyCode.Space:
+                if (isGrounded)
+                {
+                    rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                    isGrounded = false;
+                }
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
