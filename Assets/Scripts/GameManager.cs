b0c069fe9b06c3ea607390e4a9c1aafa97c737e0
Marcus Mouyat <marcus@mouyat.com>
2019-07-31 16:17:43 +1000

    worked on save system

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform player;
+    public string fileName = "GameData.xml";
+    private GameData data = new GameData();
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        Load();
+    }
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            data.position = player.position;
+            data.rotation = player.rotation;
+            data.dialogue = new string[]
+            {
+                "Insert",
+                "Some",
+                "Dialogue",
+                "Here"
+            };
+            Save(Application.dataPath + "/" + fileName);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Load(Application.dataPath + "/" + fileName);
+            //player = FindObjectOfType<Player>().transform;
+        }
+    }
+
+    public void Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(GameData));
+        var stream = new FileStream(path, FileMode.Open);
+        data = (GameData)serializer.Deserialize(stream);
+        stream.Close();
+    }
+
+    public void Save(string path)
+    {
+        var serializer = new XmlSerializer(typeof(GameData));
+        var stream = new FileStream(path, FileMode.Create);
+        serializer.Serialize(stream, data);
+        stream.Close();
+    }
+    
+    
+    
+    
+    
+}
---
+++ Assets/Scripts/SyncTransform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncTransform : NetworkBehaviour
+{
+    public float lerpRate = 15;
+
+    [SyncVar] private Vector3 syncPosition;
+    [SyncVar] private Quaternion syncRotation;
+
+    Rigidbody rigid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            rigid.rotation = Quaternion.Lerp(rigid.rotation, syncRotation, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command] void CmdSendPositionToServer(Vector3 _position)
+    {
+        syncPosition = _position;
+    }
+
+    [Command] void CmdSendRotationToServer(Quaternion _rotation)
+    {
+        syncRotation = _rotation;
+    }
+}
